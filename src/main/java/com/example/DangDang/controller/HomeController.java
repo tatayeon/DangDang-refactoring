917331a0013c21f451a7e7c079bac538277adfc5
taetae <dlaxodus1212@naver.com>
2024-09-22 18:37:28 +0900

    강아지 사진 넘기기 완료

--- src/main/java/com/example/DangDang/controller/HomeController.java
+++ src/main/java/com/example/DangDang/controller/HomeController.java
@@ -3,31 +3,31 @@ package com.example.DangDang.controller;
 import com.example.DangDang.domain.Result;
 import com.example.DangDang.domain.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String showIndex(Model model, HttpSession session) throws JsonProcessingException {
+    public String showIndex(Model model, HttpSession session) {
         User logIn = new User();
-        if(model.getAttribute("user") != null){
+        if (model.getAttribute("user") != null) {
             logIn = (User) session.getAttribute("logIn");
-
         }
-        if(session.getAttribute("In") != null){
+
+        if (session.getAttribute("In") != null) {
             session.setAttribute("In", true);
         }
+
         model.addAttribute("user", logIn);
         System.out.println("User: " + logIn.getNickName());
         String re = (String) session.getAttribute("re");
@@ -40,13 +40,24 @@ public class HomeController {
             if (session.getAttribute("re") != null) {
                 String jsonStr = (String) session.getAttribute("re");
                 Gson gson = new Gson();
-                Type type = new TypeToken<ArrayList<Result>>(){}.getType();
-                ArrayList<Result> list = gson.fromJson(jsonStr, type);
+
+                // JSON을 Map으로 변환
+                Type mapType = new TypeToken<Map<String, Object>>(){}.getType();
+                Map<String, Object> map = gson.fromJson(jsonStr, mapType);
+
+                // 'result' 키를 통해 JSON 배열 추출
+                String resultJson = (String) map.get("result");
+
+                // JSON 배열을 다시 List<Result>로 변환
+                Type listType = new TypeToken<ArrayList<Result>>(){}.getType();
+                ArrayList<Result> list = gson.fromJson(resultJson, listType);
+
                 System.out.println("list2=" + list);
                 model.addAttribute("list", list);
             }
             return "index";
         }
+
         model.addAttribute("In", false); // 로그인하지 않은 경우
         return "index";
     }
